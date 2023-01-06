b8e5563754e100315f2d363b6ab589cb00892b59
Ray Eno <rayeno@rays-mbp.home>
2023-01-06 12:03:46 +0000

    Imported local app to github

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons'
+import { robots } from './robots'
+
+ReactDOM.render(<App></App>,document.getElementById('root'));
+
+reportWebVitals();
